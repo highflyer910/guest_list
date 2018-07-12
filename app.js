232e9db2e27815b0d952463ff9a3f4e8b2996bca
Thea <tmush2014@agruni.edu.ge>
2018-07-12 20:44:36 +0400

    update

--- app.js
+++ app.js
@@ -9,16 +9,19 @@ new Vue({
 		 eventDescription: 'It\'s back! This years summer festival will be in the beautiful countryside featuring our best line up ever!'
 	  },
 	  newNameText: '',
-	  guestName: ['Max', 'Thea'],
+	  guestName: [],
 	  appStyles: {
 	  	marginTop: '25px'
-	  }
+	  },
+	  eventCapacity: 25,
+	  eventCapacityPercentage: 0
 	},
 	methods: {
 		formSubmitted: function(){
-			if(this.newNameText.length > 0){
+			if(this.newNameText.length > 0 && this.eventCapacityPercentage < 100){
 			this.guestName.push(this.newNameText)
 			this.newNameText = ''
+			this.eventCapacityPercentage = this.guestName.length / (this.eventCapacity / 100)
 		}
 	  }
 	}
